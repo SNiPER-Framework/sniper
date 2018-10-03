0220f808d9e2eaaaaa719205129d27bf6004d099
Vladimir Belov <belov@itep.ru>
2018-10-03 14:40:40 +0300

    Add setup script to set environment for run.

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+filename=`readlink -f "${BASH_SOURCE[0]}"`
+basedir=`dirname "${filename}"`
+
+export PYTHONPATH="$basedir/python:$basedir/lib${PYTHONPATH:+:${PYTHONPATH}}"
+export LD_LIBRARY_PATH="$basedir/lib${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
