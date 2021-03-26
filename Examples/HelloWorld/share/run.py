c4c8ee0ab5a6d56ce401db7b3abda4cd7d2b0f26
lintao <lintao51@gmail.com>
2021-03-26 21:52:12 +0800

    WIP: migrate python 2 to python 3.

--- Examples/HelloWorld/share/run.py
+++ Examples/HelloWorld/share/run.py
@@ -22,15 +22,15 @@ if __name__ == "__main__":
     task.property("algs").append("HelloAlg/SetAlg")
 
     x = task.find("SetAlg")
-    print "Before setting properties"
+    print("Before setting properties")
     x.show()
 
-    print
+    print()
 
     x.property("VarString").set("GOD")
-    x.property("VectorInt").set(range(6))
+    x.property("VectorInt").set(list(range(6)))
     x.property("MapStrInt").set( {"str%d"%v:v for v in range(6)} )
-    print "After setting properties"
+    print("After setting properties")
     x.show()
 
     y = HelloWorld.HelloPy("PyAlg")
