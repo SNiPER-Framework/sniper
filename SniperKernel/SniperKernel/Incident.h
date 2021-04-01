eebcd69238623d6a9dd2c4be2972c65cc9ef57a6
zoujh@ihep.ac.cn <zoujh@ihep.ac.cn>
2021-04-01 14:54:29 +0800

    add Incident_T<> and ScopedIncidentsPair

--- SniperKernel/SniperKernel/Incident.h
+++ SniperKernel/SniperKernel/Incident.h
@@ -24,28 +24,69 @@ class Task;
 
 class Incident
 {
-    public :
+public:
+    /// return the number of handlers for the incident, -1 in case of error
+    static int fire(Task &domain, const std::string &key);
 
-        /// return the number of handlers for the incident, -1 in case of error
-        static int fire(Task& domain, const std::string& key);
+    Incident(const std::string &key)
+        : m_key(key) {}
 
-        Incident(const std::string& key)
-            : m_key(key) {}
+    virtual ~Incident() {}
 
-        virtual ~Incident() {}
+    /// return the number of handlers for the incident, -1 in case of error
+    virtual int fire(Task &domain);
 
-        /// return the number of handlers for the incident, -1 in case of error
-        virtual int fire(Task& domain);
+    const std::string &name() { return m_key; }
 
-        const std::string& name() { return m_key; }
+protected:
+    std::string m_key;
 
-
-    protected :
-
-        std::string  m_key;
-
-        //not provided
-        Incident() = delete;
+    //not provided
+    Incident() = delete;
 };
+
+//fire a pair of incidents at the beginning and ending of a scope respectively
+struct ScopedIncidentsPair
+{
+    ScopedIncidentsPair(Incident &begin,
+                      Incident &end,
+                      Task &domain)
+        : m_end(end),
+          m_domain(domain)
+    {
+        begin.fire(domain);
+    }
+
+    ~ScopedIncidentsPair()
+    {
+        m_end.fire(m_domain);
+    }
+
+    Incident &m_end;
+    Task &m_domain;
+};
+
+//templated Incident that can ship additional data
+template <typename T>
+class Incident_T : public Incident
+{
+public:
+    Incident_T(const std::string &key)
+        : Incident(key) {}
+
+    Incident_T<T> &load(const T &data);
+
+    const T &payload() { return m_data; }
+
+private:
+    T m_data;
+};
+
+template <typename T>
+Incident_T<T> &Incident_T<T>::load(const T &data)
+{
+    m_data = data;
+    return *this;
+}
 
 #endif
