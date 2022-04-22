a5cfd3ef554184c259303a86d76fa3bf259c905a
zoujh <zoujh@ihep.ac.cn>
2022-04-22 20:26:10 +0800

    fix a bug for incident subtypes in sub-tasks

--- SniperKernel/src/IIncidentHandler.cc
+++ SniperKernel/src/IIncidentHandler.cc
@@ -1,17 +1,17 @@
-/* Copyright (C) 2018-2021
+/* Copyright (C) 2018-2022
    Institute of High Energy Physics and Shandong University
    This file is part of SNiPER.
- 
+
    SNiPER is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
- 
+
    SNiPER is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
- 
+
    You should have received a copy of the GNU Lesser General Public License
    along with SNiPER.  If not, see <http://www.gnu.org/licenses/>. */
 
@@ -29,7 +29,7 @@ IIncidentHandler::IIncidentHandler(ExecUnit &domain)
 {
 }
 
-IIncidentHandler::IIncidentHandler(ExecUnit* domain)
+IIncidentHandler::IIncidentHandler(ExecUnit *domain)
     : m_domain(*domain),
       m_name("IncidentHandler"),
       m_scope(domain->scope() + domain->objName() + ':'),
@@ -39,25 +39,25 @@ IIncidentHandler::IIncidentHandler(ExecUnit* domain)
 
 IIncidentHandler::~IIncidentHandler()
 {
-    std::list<std::string>::reverse_iterator it = m_msg.rbegin();
-    while ( it != m_msg.rend() ) {
-        this->unregist(*it);
-        it = m_msg.rbegin();
+    for (auto it = m_msg.rbegin(); it != m_msg.rend(); ++it)
+    {
+        IncidentMgr::instance(m_id).unregist(*it, this);
     }
 }
 
-bool IIncidentHandler::regist(const std::string& incident)
+bool IIncidentHandler::regist(const std::string &incident)
 {
-    std::string msg = m_domain.isRoot()
-        ? incident : (m_domain.objName() + ':' + incident);
+    std::string msg = m_domain.scope() + m_domain.objName() + ':' + incident;
     std::list<std::string>::iterator it =
         std::find(m_msg.begin(), m_msg.end(), msg);
 
-    if ( it == m_msg.end() ) {
+    if (it == m_msg.end())
+    {
         m_msg.push_back(msg);
         IncidentMgr::instance(m_id).regist(msg, this);
     }
-    else {
+    else
+    {
         LogInfo << "ignore already registered incident: "
                 << incident << std::endl;
     }
@@ -65,13 +65,15 @@ bool IIncidentHandler::regist(const std::string& incident)
     return true;
 }
 
-void IIncidentHandler::unregist(const std::string& incident)
+void IIncidentHandler::unregist(const std::string &incident)
 {
+    std::string msg = m_domain.scope() + m_domain.objName() + ':' + incident;
     std::list<std::string>::iterator it =
-        std::find(m_msg.begin(), m_msg.end(), incident);
-    if ( it != m_msg.end() ) {
+        std::find(m_msg.begin(), m_msg.end(), msg);
+    if (it != m_msg.end())
+    {
         m_msg.erase(it);
-        IncidentMgr::instance(m_id).unregist(incident, this);
+        IncidentMgr::instance(m_id).unregist(msg, this);
     }
 }
 
@@ -79,7 +81,8 @@ void IIncidentHandler::listening()
 {
     std::cout << "listening:";
     std::list<std::string>::iterator it = m_msg.begin();
-    while ( it != m_msg.end() ) {
+    while (it != m_msg.end())
+    {
         std::cout << "  " << *it;
         ++it;
     }
--- SniperKernel/src/IncidentMgr.cc
+++ SniperKernel/src/IncidentMgr.cc
@@ -1,17 +1,17 @@
-/* Copyright (C) 2018-2021
+/* Copyright (C) 2018-2022
    Institute of High Energy Physics and Shandong University
    This file is part of SNiPER.
- 
+
    SNiPER is free software: you can redistribute it and/or modify
    it under the terms of the GNU Lesser General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
- 
+
    SNiPER is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
- 
+
    You should have received a copy of the GNU Lesser General Public License
    along with SNiPER.  If not, see <http://www.gnu.org/licenses/>. */
 
@@ -56,11 +56,11 @@ void IncidentMgr::release()
     delete ps_mgrMap;
 }
 
-int IncidentMgr::handle(Incident &incident)
+int IncidentMgr::handle(ExecUnit &domain, Incident &incident)
 {
-    //LogTest << "Processing incident " << incident.name() << std::endl;
     int count = 0;
-    IncidentMap::iterator ii = m_map.find(incident.name());
+    std::string key = domain.scope() + domain.objName() + ':' + incident.name();
+    IncidentMap::iterator ii = m_map.find(key);
     if (ii != m_map.end())
     {
         HandlerList &handlers = ii->second;
